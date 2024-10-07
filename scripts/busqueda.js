3ddbfa40e4d06cd9ecc19138ce8b06a059878746
TOBIAS ZASELSKY <47435897@ort>
2024-10-07 12:11:35 -0300

    busque en google

---
+++ scripts/busqueda.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import https from "https";
+
+const buscar = (query) => {
+    console.log("llego", query);
+
+    let data = JSON.stringify({
+        "q": query,
+        "gl": "ar",
+        "hl": "es-419",
+        "tbs": "qdr:y"
+    });
+
+    let config = {
+        method: 'post',
+        url: 'https://google.serper.dev/search',
+        headers: { 
+            'X-API-KEY': 'c53d59c25876d584593b303eb26a81bd7579734c', 
+            'Content-Type': 'application/json'
+        },
+        data: data,
+        httpsAgent: new https.Agent({  
+            rejectUnauthorized: false 
+        })
+    };
+
+    return axios(config) // Asegúrate de devolver la promesa
+        .then((response) => {
+            console.log("30 BUSQUEDA",response);
+            return JSON.stringify(response.data.organic); // Devuelve la respuesta
+        })
+        .catch((error) => {
+            console.log(error);
+            throw new Error("Error en la búsqueda"); // Lanza un error para manejarlo
+        });
+};
+
+export default buscar;
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import buscar from "./scripts/busqueda.js";
+
 const app = express();
-const cors = require('cors');
-const axios = require("axios");
 
 app.use(cors());
 
 app.get("/api", (req, res) => {
-  const tweet = "RECORD DE PETROLEO\nEl petroleo paso a ser el 3er producto mas exportado de Argentina, despues de la soja y el maíz. La produccion alcanzo un RECORD y volvio a niveles historicos del 2004";
+  const tweet = "MILEI SOS UN SIGMA\nLa inflacion de septiembre es de 0%";
   
   palabrasClaves(tweet)
     .then(data => {
       console.log(data.response);
-      res.json({ resultado: data.response });
+      const paginas = buscar("Milei anuncia inflación cero en septiembre"); // Asegúrate de que buscar devuelva una promesa
+      console.log("17", data.response);
+      return paginas;
+    })
+    .then(paginas => {
+      console.log("21", paginas);
+      res.json({ resultado: paginas });
     })
     .catch(error => {
       console.error(error);
@@ -22,7 +30,7 @@ app.get("/api", (req, res) => {
 const palabrasClaves = (tweet) => {
   return axios.post("http://localhost:11434/api/generate", {
     "model": "llama3.2",
-    "prompt": "Te voy a mandar una noticia esparcida por redes sociales. tu rol como modelo de lenguaje es crear una busqueda de google con el que puede conseguir informacion sobre esta noticia. Responde con una sola busqueda posible\n\n" + tweet,
+    "prompt": "Te voy a mandar una noticia esparcida por redes sociales. tu rol como modelo de lenguaje es crear una busqueda de google con el que puede conseguir informacion sobre esta noticia.Toma en cuenta que la mayoria son de Argentina y actuales. La respuesta debe estar conformada UNICAMENTE CON LA BUSQUEDA DE GOOGLE. Tiene que ser una sola busqueda posible\n\n" + tweet,
     "stream": false
   })
   .then(res => {
