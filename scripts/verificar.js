7f122ae89ff1e97583e06696fb3e73ebb936c2a8
TOBIAS ZASELSKY <47435897@ort>
2024-11-22 11:35:39 -0300

    anda piola

--- scripts/verificar.js
+++ scripts/verificar.js
@@ -1,15 +1,37 @@
 import axios from "axios";
 
+const diarios = [
+    "https://www.clarin.com",
+    "https://www.lanacion.com.ar",
+    "https://www.pagina12.com.ar",
+    "https://www.cronista.com",
+    "https://www.lavoz.com.ar",
+    "https://www.diariopopular.com.ar",
+    "https://www.perfil.com",
+    "https://www.eldia.com",
+    "https://www.eltribuno.com",
+    "https://www.tiempoar.com.ar",
+    "https://www.ambito.com",
+    "https://www.infobae.com",
+    "https://www.cronica.com.ar",
+    "https://www.lanueva.com",
+    "https://www.ellitoral.com",
+    "https://www.elmundo.com.ar",
+    "https://www.rionegro.com.ar",
+    "https://www.elsol.com.ar"
+]
+
 const verificar = (afirmacion, paginas) => {
   // Convierte el string en un objeto JSON
 
   console.log("6VERIFICAR: AFIRMACION: " + afirmacion);
   // Genera el contenido de las páginas para el prompt
 
-  console.log("9VERIFICAR: PAGINAS ", paginas);
-
-
-  const paginasText = paginas.organic.map(pagina => {
+  
+  const paginasFiltradas = filtrarPaginas(paginas.organic)
+  console.log("9VERIFICAR: PAGINAS ", paginasFiltradas);
+  
+  const paginasText = paginasFiltradas.map(pagina => {
     
     if (pagina.position <= 5) {
       console.log("PAGINAS ", pagina.title);
@@ -22,7 +44,7 @@ const verificar = (afirmacion, paginas) => {
   // Crea el prompt con la información
   const prompt = `Voy a darte 5 articulos con titulo y descripcion, necesito que me digas si alguno verifica la siguiente afirmacion: ${afirmacion}.\nNecesito que respondas con una sola palabra. Si algun articulo verifica la afirmacion responde: 'VERDADERO'. Si los articulos NO LO VERIFICAN o dicen que es falso responde unicamente: 'FALSO'.\n
   ${paginasText}
-  Compara la información del tweet con los resultados proporcionados. Si los datos de las fuentes contradicen el tweet o si no hay suficiente información, responde 'falso'. Si los datos respaldan completamente la afirmación del tweet, responde 'verdadero'. Si hay discrepancias en fechas, nombres, eventos o datos importantes, responde 'falso'. Responde únicamente con 'verdadero' o 'falso' sin explicaciones adicionales.`;
+  Compara la información del tweet con los resultados proporcionados. Si los datos de las fuentes contradicen el tweet o si no hay suficiente información (al menos 2 articulos que lo avalen), responde 'falso'. Si los datos respaldan completamente la afirmación del tweet, responde 'verdadero'. Si hay discrepancias en fechas, nombres, eventos o datos importantes, responde 'falso'. Responde únicamente con 'verdadero' o 'falso' sin explicaciones adicionales.`;
 
   // Llamada a la API
   return axios.post("http://localhost:11434/api/generate", {
@@ -38,5 +60,16 @@ const verificar = (afirmacion, paginas) => {
     throw new Error("Error en la llamada a la API externa"); // Lanza el error para ser manejado en el manejador de la ruta
   });
 };
+
+const filtrarPaginas = (paginas) => {
+  const paginasSinX = paginas.filter(pagina => !pagina.link.includes("x.com" || "instagram.com" || "twitter.com"))
+  
+  const paginasReordenadas = [
+    ...paginasSinX.filter(pagina => diarios.includes(pagina.link)), // Páginas que están en el array de diarios
+    ...paginasSinX.filter(pagina => !diarios.includes(pagina.link)) // Páginas que no están en el array de diarios
+  ];
+
+  return paginasReordenadas
+}
 
 export default verificar;
--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get("/api", (req, res) => {
 
 const palabrasClaves = (tweet) => {
 
-  const prompt = "Extrae las palabras clave del siguiente texto y con ellas forma una oracion que permita su busqueda en google, enfocándote en nombres de personas, lugares, eventos, fechas, y términos importantes relacionados con hechos verificables. Las palabras clave deben ser aquellas que permitan realizar una búsqueda en Google para comprobar la veracidad del texto. Asegúrate de incluir solo los términos más relevantes y no incluir palabras comunes o irrelevantes. Unicamente responde con las palabras claves extraidas del tweet y convertilas en una unica oracion que permita buscarla en google. NO RESPONDAS MAS QUE LA UNICA ORACION. SI NO TIENE PALABRAS CLAVE DEVOLVE SOLO EL TEXTO DEL TWEET ORIGINAL\n" + tweet;
+  const prompt = "Extrae las palabras clave del siguiente texto y con ellas forma una oracion que permita su busqueda en google, enfocándote en nombres de personas, lugares, eventos, fechas, y términos importantes relacionados con hechos verificables. Las palabras clave deben ser aquellas que permitan realizar una búsqueda en Google para comprobar la veracidad del texto. Asegúrate de incluir solo los términos más relevantes. Unicamente responde con las palabras claves extraidas del tweet y convertilas en una unica oracion que permita buscarla en google. NO RESPONDAS MAS QUE CON LA UNICA ORACION. SI NO TIENE PALABRAS CLAVE DEVOLVE SOLO EL TEXTO DEL TWEET ORIGINAL\n" + tweet;
 
 
   return axios.post("http://localhost:11434/api/generate", {
